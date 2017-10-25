db62a64b89dc9eff15681b4ed904f24de6dd55b2
jackyho112 <jackyho112dev@gmail.com>
2017-10-24 22:30:47 -0700

    Finish the first few exercises

---
+++ basics.sql
@@ -0,0 +1,11 @@
+-- select every car record from the car table
+SELECT * FROM car;
+
+-- select every car's brand from the car table
+SELECT brand FROM car;
+
+-- select every car's model and price from the car table
+SELECT model, price FROM car;
+
+-- select every car from the car table where the production year is 1999
+SELECT * FROM car WHERE production_year = 1999;
